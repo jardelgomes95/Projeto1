793ed934f621a5a5086fc74cc4fa301f934711e3
arthurvncs <arthurxd.vncs@gmail.com>
2021-05-28 19:35:50 -0300

    repositorio

---
+++ vendas/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from .views import kangoodanceCreateView, funcionalCreateView, muaythaiCreateView, frequenciaCreateView, \
+    frequenciaListView, financeiroListView, matriculaUpdateView
+from .views import matriculaCreateView, outros_servicoCreateView, financeiroCreateView, pilatesCreateView, \
+    fitdanceCreateView, matriculaListView
+
+urlpatterns = [
+    path('vendas/aluno', matriculaCreateView.as_view(), name="Matricula"),
+    path('vendas/outrosservicos', outros_servicoCreateView.as_view(), name="Outros_Serviços"),
+    path('vendas/financeiro', financeiroCreateView.as_view(), name="Financeiro"),
+    path('vendas/pilates', pilatesCreateView.as_view(), name="Pilates"),
+    path('vendas/fitdance', fitdanceCreateView.as_view(), name="Fitdance"),
+    path('vendas/kangoo', kangoodanceCreateView.as_view(), name="Kangoo"),
+    path('vendas/funcional', funcionalCreateView.as_view(), name="Funcional"),
+    path('vendas/muaythai', muaythaiCreateView.as_view(), name="Muaythai"),
+    path('vendas/frequencia', frequenciaCreateView.as_view(), name="Frequencia"),
+    path('listar/cliente', matriculaListView.as_view(), name="Listar_Matricula"),
+    path('listar/frequencia', frequenciaListView.as_view(), name="Listar_Frequencia"),
+    path('listar/financeiro', financeiroListView.as_view(), name="Financeiro"),
+
+
+    path('editar/matricula/<int:pk>/', matriculaUpdateView.as_view(), name="Editar_Matricula"),
+]
---
+++ vendas/views.py
@@ -0,0 +1,112 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView
+
+from .models import matricula, outros_servico, financeiro, pilates, fitdance, kangoodance, funcional, muaythai, \
+    frequencia
+
+
+# Create your views here.
+
+########## CREATEVIEW ##########
+
+
+class matriculaCreateView(CreateView):
+    model = matricula
+    template_name = 'cliente.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('Listar_Matricula')
+
+
+class outros_servicoCreateView(CreateView):
+    model = outros_servico
+    template_name = 'outros_servicos.html'
+
+    fields = '__all__'
+
+
+class financeiroCreateView(CreateView):
+    model = financeiro
+    template_name = 'financeiro.html'
+
+    fields = '__all__'
+
+
+class pilatesCreateView(CreateView):
+    model = pilates
+    template_name = 'pilates.html'
+
+    fields = '__all__'
+
+
+class fitdanceCreateView(CreateView):
+    model = fitdance
+    template_name = 'fit_dance.html'
+
+    fields = '__all__'
+
+
+
+class kangoodanceCreateView(CreateView):
+    model = kangoodance
+    template_name = 'kangoo.html'
+
+    fields = '__all__'
+
+
+class funcionalCreateView(CreateView):
+    model = funcional
+    template_name = 'kangoo.html'
+
+    fields = '__all__'
+
+
+
+class muaythaiCreateView(CreateView):
+    model = muaythai
+    template_name = 'muay_thai.html'
+
+    fields = '__all__'
+
+
+
+class frequenciaCreateView(CreateView):
+    model = frequencia
+    template_name = 'frequencia.html'
+
+    fields = '__all__'
+
+
+
+class matriculaListView(ListView):
+    model = matricula
+    template_name = 'listar/listarcliente.html'
+    paginate_by = 10
+
+
+class financeiroListView(ListView):
+    model = financeiro
+    template_name = 'listar/listarfinanceiro.html'
+    paginate_by = 10
+
+
+class frequenciaListView(ListView):
+    model = frequencia
+    template_name = 'listar/listarfrequencia.html'
+    paginate_by = 10
+
+
+
+########## UPDATEVIEW ##########
+
+class matriculaUpdateView(UpdateView):
+    model = matricula
+    template_name = 'cliente.html'
+
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('Listar_Matricula')
+
